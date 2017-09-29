75c957f516e593078ab2481bf748ad17f1d51008
Estevão Lucena <alves_estevao@hotmail.com>
2017-09-29 15:43:16 -0300

    Adicionando model Avaliacao e Categoria

---
+++ src/model/Avaliacao.java
@@ -0,0 +1,61 @@
+package model;
+
+public class Avaliacao
+	{
+		int notaAcessoCadeirante;
+		int notaSanitarioCadeirante;
+		int notaInstrucaoBraile;
+		int notaSinalizacaoPiso;
+		int notaGeral;
+		String Comentario;
+		
+		
+		public int getNotaAcessoCadeirante()
+			{
+				return notaAcessoCadeirante;
+			}
+		public void setNotaAcessoCadeirante(int notaAcessoCadeirante)
+			{
+				this.notaAcessoCadeirante = notaAcessoCadeirante;
+			}
+		public int getNotaSanitarioCadeirante()
+			{
+				return notaSanitarioCadeirante;
+			}
+		public void setNotaSanitarioCadeirante(int notaSanitarioCadeirante)
+			{
+				this.notaSanitarioCadeirante = notaSanitarioCadeirante;
+			}
+		public int getNotaInstrucaoBraile()
+			{
+				return notaInstrucaoBraile;
+			}
+		public void setNotaInstrucaoBraile(int notaInstrucaoBraile)
+			{
+				this.notaInstrucaoBraile = notaInstrucaoBraile;
+			}
+		public int getNotaSinalizacaoPiso()
+			{
+				return notaSinalizacaoPiso;
+			}
+		public void setNotaSinalizacaoPiso(int notaSinalizacaoPiso)
+			{
+				this.notaSinalizacaoPiso = notaSinalizacaoPiso;
+			}
+		public int getNotaGeral()
+			{
+				return notaGeral;
+			}
+		public void setNotaGeral(int notaGeral)
+			{
+				this.notaGeral = notaGeral;
+			}
+		public String getComentario()
+			{
+				return Comentario;
+			}
+		public void setComentario(String comentario)
+			{
+				Comentario = comentario;
+			}		
+	}
