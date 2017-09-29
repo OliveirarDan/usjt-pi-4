6247b8bfb57eca8de9dc2d0051b020f6fd298498
Estevão Lucena <alves_estevao@hotmail.com>
2017-09-29 15:13:21 -0300

    Adicionando Usuario controller e service

---
+++ src/service/UsuarioService.java
@@ -0,0 +1,29 @@
+package service;
+
+import dao.UsuarioDAO;
+import model.Usuario;;
+
+public class UsuarioService
+	{
+		UsuarioDAO dao = new UsuarioDAO();
+
+		public void criar(Usuario usuario)
+			{
+				dao.criar(usuario);
+			}
+
+		public void atualizar(Usuario usuario)
+			{
+				dao.atualizar(usuario);
+			}
+
+		public void excluir(Usuario usuario)
+			{
+				dao.excluir(usuario);
+			}
+
+		public Usuario carregar(int id)
+			{
+				return dao.carregar(id);
+			}
+	}
